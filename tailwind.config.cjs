2c926363b229d12c84897fabfbd23443fe5061c2
Abubakri Sherif <saykeed@gmail.com>
2022-08-18 21:10:35 +0100

    initial commit after seting up

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+
+/** @type {import('tailwindcss').Config} */ 
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+				primary: 'var(--primary)',
+        background: 'var(--backGround)',
+        white: 'var(--white)',
+				blue: 'var(--blue)',
+				light_grey: 'var(--light_grey)',
+				divider: 'var(--divider)'
+			}
+    },
+  },
+  plugins: [],
+}
