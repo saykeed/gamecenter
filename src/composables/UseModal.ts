cbcd947622cff1f280fe9558ca600a2dd4e321e0
Abubakri Sherif <saykeed@gmail.com>
2022-08-29 19:45:56 +0100

    fixed type error

--- src/composables/UseModal.ts
+++ src/composables/UseModal.ts
@@ -2,11 +2,11 @@ import {ref} from 'vue'
 import Cam from '../Interface/cam'
 
 const modalStatus = ref(false)
-const modalOptions = ref<Cam[]>()
+const modalOptions = ref<any[]>()
 
 export const useModal = () => {
 
-    const openModal = (arr:Cam[]) => {
+    const openModal = (arr:any) => {
         modalOptions.value = arr
         modalStatus.value = true
     }
