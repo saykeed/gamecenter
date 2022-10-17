164dccab8bcc3293a7e1ccd8003a0ece2c1284fa
Abubakri Sherif <saykeed@gmail.com>
2022-10-17 18:09:29 +0100

    added turn server to receiver config aswell

--- src/composables/tictactoe/HandleVideoChat.ts
+++ src/composables/tictactoe/HandleVideoChat.ts
@@ -63,15 +63,38 @@ const configuration = {
     // ]
 }
 const joinConfig = {
-    iceServers: [
-      {
-        urls: [
-          'stun:stun1.l.google.com:19302',
-          'stun:stun2.l.google.com:19302',
-        ],
-      },
+    // iceServers: [
+    //   {
+    //     urls: [
+    //       'stun:stun1.l.google.com:19302',
+    //       'stun:stun2.l.google.com:19302',
+    //     ],
+    //   },
+    // ],
+    // iceCandidatePoolSize: 10,
+	iceServers: [
+        // {
+        //   urls: [
+        //     'stun:stun1.l.google.com:19302',
+        //     'stun:stun2.l.google.com:19302',
+        //   ],
+        // },
+        {
+          urls: "turn:openrelay.metered.ca:80",
+          username: "openrelayproject",
+          credential: "openrelayproject"
+        },
+        {
+          urls: "turn:openrelay.metered.ca:443",
+          username: "openrelayproject",
+          credential: "openrelayproject"
+        },
+        {
+          urls: "turn:openrelay.metered.ca:443?transport=tcp",
+          username: "openrelayproject",
+          credential: "openrelayproject"
+        }
     ],
-    iceCandidatePoolSize: 10,
 }
 
 export const useVideoChat = () => {
