66389dcc88c2fd4d12479ff78b95fabab7917286
Abubakri Sherif <saykeed@gmail.com>
2022-11-08 17:22:43 +0100

    updated

--- src/composables/tictactoe/HandleVideoChat.ts
+++ src/composables/tictactoe/HandleVideoChat.ts
@@ -1,4 +1,4 @@
-import {ref} from 'vue'
+import { ref } from 'vue'
 import { useSocket } from '../socket/UseSocket'
 import { useAlert } from '../UseAlert'
 import { useModal } from '../UseModal'
@@ -7,7 +7,7 @@ import { useLoader } from '../UseLoader';
 
 const { socket } = useSocket()
 const { openAlert } = useAlert()
-const { openModal } =  useModal()
+const { openModal } = useModal()
 const { openLoader, closeLoader } = useLoader()
 
 
@@ -23,130 +23,129 @@ let overSeerPeerConn = ref<RTCPeerConnection>()
 // let remoteStream = new MediaStream()
 // const configuration = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]}
 const configuration = {
-    iceServers: [
-        {
-          urls: [
-            'stun:stun1.l.google.com:19302',
-            'stun:stun2.l.google.com:19302',
-          ],
-        },
-        {
-          urls: "turn:openrelay.metered.ca:80",
-          username: "openrelayproject",
-          credential: "openrelayproject"
-        },
-        {
-          urls: "turn:openrelay.metered.ca:443",
-          username: "openrelayproject",
-          credential: "openrelayproject"
-        },
-        {
-          urls: "turn:openrelay.metered.ca:443?transport=tcp",
-          username: "openrelayproject",
-          credential: "openrelayproject"
-        }
-    ],
-    iceCandidatePoolSize: 10,
+	iceServers: [
+		{
+			urls: [
+				'stun:stun1.l.google.com:19302',
+				'stun:stun2.l.google.com:19302',
+			],
+		},
+		{
+			urls: "turn:openrelay.metered.ca:80",
+			username: "openrelayproject",
+			credential: "openrelayproject"
+		},
+		{
+			urls: "turn:openrelay.metered.ca:443",
+			username: "openrelayproject",
+			credential: "openrelayproject"
+		},
+		{
+			urls: "turn:openrelay.metered.ca:443?transport=tcp",
+			username: "openrelayproject",
+			credential: "openrelayproject"
+		}
+	],
+	iceCandidatePoolSize: 10,
 }
 const joinConfig = {
-    iceServers: [
-      {
-        urls: [
-          'stun:stun1.l.google.com:19302',
-          'stun:stun2.l.google.com:19302',
-        ],
-      },
-    ],
-    iceCandidatePoolSize: 10,
+	iceServers: [
+		{
+			urls: [
+				'stun:stun1.l.google.com:19302',
+				'stun:stun2.l.google.com:19302',
+			],
+		},
+	],
+	iceCandidatePoolSize: 10,
 	// iceServers: [
-    //     // {
-    //     //   urls: [
-    //     //     'stun:stun1.l.google.com:19302',
-    //     //     'stun:stun2.l.google.com:19302',
-    //     //   ],
-    //     // },
-    //     {
-    //       urls: "turn:openrelay.metered.ca:80",
-    //       username: "openrelayproject",
-    //       credential: "openrelayproject"
-    //     },
-    //     {
-    //       urls: "turn:openrelay.metered.ca:443",
-    //       username: "openrelayproject",
-    //       credential: "openrelayproject"
-    //     },
-    //     {
-    //       urls: "turn:openrelay.metered.ca:443?transport=tcp",
-    //       username: "openrelayproject",
-    //       credential: "openrelayproject"
-    //     }
-    // ],
+	//     // {
+	//     //   urls: [
+	//     //     'stun:stun1.l.google.com:19302',
+	//     //     'stun:stun2.l.google.com:19302',
+	//     //   ],
+	//     // },
+	//     {
+	//       urls: "turn:openrelay.metered.ca:80",
+	//       username: "openrelayproject",
+	//       credential: "openrelayproject"
+	//     },
+	//     {
+	//       urls: "turn:openrelay.metered.ca:443",
+	//       username: "openrelayproject",
+	//       credential: "openrelayproject"
+	//     },
+	//     {
+	//       urls: "turn:openrelay.metered.ca:443?transport=tcp",
+	//       username: "openrelayproject",
+	//       credential: "openrelayproject"
+	//     }
+	// ],
 }
 
 export const useVideoChat = () => {
-	
-    const constraints = {
-        'video': true,
-        'audio': true
-    }
 
-    async function getConnectedDevices(type:string) {
-        if(navigator.mediaDevices) {
-            const devices = await navigator.mediaDevices.enumerateDevices();
-            return devices.filter(device => device.kind === type)
-        } else {
-            openAlert('can not open media devices')
-        }
-        
-    }
+	const constraints = {
+		'video': true,
+		'audio': true
+	}
 
-    async function openCamera(cameraId:string) {
-        const constraints = {
-            'audio': {'echoCancellation': true},
-            'video': {
-                'deviceId': cameraId
-                }
-            }
-    
-        return await navigator.mediaDevices.getUserMedia(constraints);
-    }
+	async function getConnectedDevices(type: string) {
+		if (navigator.mediaDevices) {
+			const devices = await navigator.mediaDevices.enumerateDevices();
+			return devices.filter(device => device.kind === type)
+		} else {
+			openAlert('can not open media devices')
+		}
 
-    const getLocalSteam = async () => {
-        navigator.mediaDevices.getUserMedia(constraints)
-        .then(() => {
-            getConnectedDevices('videoinput').then(data => {
-                openModal(data)
-            })
-        })
-        .catch(() => {
-            openAlert('Could not access media devices')
-        })
+	}
 
-		
-    }
+	async function openCamera(cameraId: string) {
+		const constraints = {
+			'audio': { 'echoCancellation': true },
+			'video': {
+				'deviceId': cameraId
+			}
+		}
 
-    const selectDevice = (selectedCam:MediaDeviceInfo) => {
-        openCamera(selectedCam.deviceId).then(dataStream => {
-            stream.value = dataStream
-            // if(userType.value === 'sender') {
-            //     makeCall()
-            // } else {
-            //     receiveCall()
-            // }
-        })
-    }
+		return await navigator.mediaDevices.getUserMedia(constraints);
+	}
 
-    
+	const getLocalSteam = async () => {
+		navigator.mediaDevices.getUserMedia(constraints)
+			.then(() => {
+				getConnectedDevices('videoinput').then(data => {
+					openModal(data)
+				})
+			})
+			.catch(() => {
+				openAlert('Could not access media devices')
+			})
 
-    async function makeCall() {
-		
+
+	}
+
+	const selectDevice = (selectedCam: MediaDeviceInfo) => {
+		openCamera(selectedCam.deviceId).then(dataStream => {
+			stream.value = dataStream
+			// if(userType.value === 'sender') {
+			//     makeCall()
+			// } else {
+			//     receiveCall()
+			// }
+		})
+	}
+
+
+
+	async function makeCall() {
+
 		socket.value?.emit('outgoingOffer', peer.id)
 		openLoader('Waiting for opponent to receive call')
 		peer.on("connection", (conn) => {
 			conn.on("data", (data) => {
 				closeLoader()
 				alert(data);
-				
 			});
 		});
 		socket.value?.on('callRejected', () => {
@@ -154,13 +153,10 @@ export const useVideoChat = () => {
 			openAlert('call rejected')
 			return;
 		})
-		
-		
-		
-       
-    }
-    
-    const handleIncomingWebrtcData = () => {
+
+	}
+
+	const handleIncomingWebrtcData = () => {
 		peer = new Peer(`saykeed-game-center-${new Date().getTime()}`);
 
 		peer.on("connection", (conn) => {
@@ -172,33 +168,32 @@ export const useVideoChat = () => {
 			});
 		});
 
-		peer.on("call", (call) => {
-			try {
-				getLocalSteam()
-				call.answer(stream.value); 
-				call.on("stream", (remoteVid) => {
+		peer.on('call', (call) => {
+			navigator.mediaDevices.getUserMedia({video: true, audio: true})
+			  .then((localStream) => {
+				call.answer(localStream);
+				videoCallStatus.value = true
+				call.on('stream', (remoteVid) => {
 					remoteStream.value = remoteVid
 				});
-				videoCallStatus.value = true
-			} catch (error) {
-				videoCallStatus.value = false
-				openAlert('error')
-			}
-			
-		});
-		
+			  })
+			  .catch((err) => {
+				console.error('Failed to get local stream', err);
+			  });
+		  });
+
 		socket.value?.on('incomingOffer', async (id) => {
 			if (id) {
 				let accept = confirm('Opponent requests a video call')
-				if(accept) {
+				if (accept) {
 					startCall(id)
 				} else {
 					socket.value?.emit('rejectCall')
 				}
 			}
 		})
-		
-    }
+
+	}
 
 	const startCall = async (id: any) => {
 		const conn = peer.connect(id)
@@ -208,23 +203,25 @@ export const useVideoChat = () => {
 		conn.on("open", () => {
 			conn.send("receiver");
 		});
-		try {
-			getLocalSteam()
+
+		navigator.mediaDevices.getUserMedia({video: true, audio: true})
+		.then((localStream) => {
+			stream.value = localStream
 			videoCallStatus.value = true
-			const call = peer.call(id, stream.value!);
-			call.on("stream", (remoteVid) => {
+			let call = peer.call(id, localStream);
+			call.on('stream', (remoteVid) => {
 				remoteStream.value = remoteVid
 			});
-		} catch (error) {
-			videoCallStatus.value = false
-			openAlert('error')
-		}
-		
+		})
+		.catch((err) => {
+			console.log('Failed to get local stream', err);
+		});
+
 	}
 
 
 
-    return { selectDevice, stream, remoteStream, handleIncomingWebrtcData, videoCallStatus, makeCall}
+	return { selectDevice, stream, remoteStream, handleIncomingWebrtcData, videoCallStatus, makeCall }
 }
 
 
@@ -235,40 +232,40 @@ const videoStatus = ref(true)
 const minimizeVideoChat = ref(false)
 
 export const useVideoChatOptions = () => {
-    const toggleVideoChatOptions = () => {
-        videoChatOptions.value = !videoChatOptions.value
-    }
+	const toggleVideoChatOptions = () => {
+		videoChatOptions.value = !videoChatOptions.value
+	}
 
-    const controlAudio = (stream:MediaStream | undefined) => {
-        audioStatus.value = !audioStatus.value
-        stream?.getTracks().forEach(track => {
-            if (track.kind === 'audio') {
-              track.enabled = audioStatus.value
-            //   console.log(track)
-            }
-        })
-    }
+	const controlAudio = (stream: MediaStream | undefined) => {
+		audioStatus.value = !audioStatus.value
+		stream?.getTracks().forEach(track => {
+			if (track.kind === 'audio') {
+				track.enabled = audioStatus.value
+				//   console.log(track)
+			}
+		})
+	}
 
-    const controlVideo = (stream:MediaStream | undefined) => {
-        videoStatus.value = !videoStatus.value
-        stream?.getTracks().forEach(track => {
-            if (track.kind === 'video') {
-              track.enabled = videoStatus.value
-            //   console.log(track)
-            }
-        })
-    }
+	const controlVideo = (stream: MediaStream | undefined) => {
+		videoStatus.value = !videoStatus.value
+		stream?.getTracks().forEach(track => {
+			if (track.kind === 'video') {
+				track.enabled = videoStatus.value
+				//   console.log(track)
+			}
+		})
+	}
 
-    const controlVideoChatLayout = () => {
-        minimizeVideoChat.value = !minimizeVideoChat.value
-    }
+	const controlVideoChatLayout = () => {
+		minimizeVideoChat.value = !minimizeVideoChat.value
+	}
 
 	const hangUp = () => {
 		stream.value?.getTracks().forEach(track => track.stop())
-		if(remoteStream.value != undefined) {
+		if (remoteStream.value != undefined) {
 			remoteStream.value?.getTracks().forEach(track => track.stop())
 		}
-		if(overSeerPeerConn.value != undefined) {
+		if (overSeerPeerConn.value != undefined) {
 			overSeerPeerConn.value?.close()
 		}
 		stream.value = undefined
@@ -276,7 +273,7 @@ export const useVideoChatOptions = () => {
 		videoCallStatus.value = false
 	}
 
-    return { videoChatOptions, toggleVideoChatOptions, audioStatus, videoStatus, controlAudio, controlVideo, minimizeVideoChat, controlVideoChatLayout, hangUp }
+	return { videoChatOptions, toggleVideoChatOptions, audioStatus, videoStatus, controlAudio, controlVideo, minimizeVideoChat, controlVideoChatLayout, hangUp }
 }
 
 
@@ -329,7 +326,7 @@ export const useVideoChatOptions = () => {
 // 		openAlert('ice connection disconnected')
 // 	} else if(peerConn.iceConnectionState === 'connected') {
 // 		openAlert('ice connected')
-// 	} 
+// 	}
 // }
 
 // const remoteTrackListener = (peerConn:RTCPeerConnection) => {
